d9149a0eaefe3f037ac9e6bc74bb9c64a2d7e1c1
Leonardo Sena <lgomesgabriel@hotmail.com>
2022-10-24 13:47:55 -0300

    Game app using angular

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { title } from 'process';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() username = '';
+  @Input() content = '';
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
